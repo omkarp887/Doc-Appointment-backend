38d42622e8dff87505162b70510495e88a2369ef
Omkar Patil <omkarp666@gmail.com>
2023-05-05 11:31:14 +0530

    created middle ware and model for login register

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const dbconfig = require("./config/dbconfig");
+const userRoute = require("./routes/userRoute");
+app.use(express.json());
 
+app.use("/api/user", userRoute);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
